8029133542166d2497e4e9ae52f4ffdc5bdf076c
cygon23 <rweyemamu088@gmail.com>
2025-12-01 09:02:07 +0300

    changing the background
    Signed-off-by: cygon23 <rweyemamu088@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ConvexClientProvider>
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
           <Navbar />
           {children}
         </ConvexClientProvider>
